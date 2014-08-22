39fa90120a2130ba779b3ee3aea4a55d21f8f429
Yura Griga <grigach@gmail.com>
2014-08-22 22:09:03 +0300

    recaptcha

--- models/NotificationForm.php
+++ models/NotificationForm.php
@@ -18,9 +18,29 @@ class NotificationForm extends CFormModel
         return [
             ['email', 'email'],
             ['email, fullname, subject, message, phone','filter','filter'=>array($obj=new CHtmlPurifier(),'purify')],
-            ['captcha', 'captcha', 'allowEmpty' => !Yii::app()->user->isGuest || !CCaptcha::checkRequirements(),
-            'captchaAction'=>'site/captcha'],
+            ['captcha', 'recaptcha'],
         ];
+    }
+
+    /**
+     * check google recaptcha
+     * check the password against the pattern requested
+     * by the strength parameter
+     * This is the 'passwordStrength' validator as declared in rules().
+     */
+    public function recaptcha($attribute,$params)
+    {
+        require_once(Yii::getPathOfAlias('ext.recaptcha.recaptchalib').'.php');
+        $privateKey = Config::get('apiKeys.recaptcha.private');
+        $resp = recaptcha_check_answer ($privateKey,
+            $_SERVER["SERVER_NAME"],
+            $this->attributes[$attribute]['challenge'],
+            $this->attributes[$attribute]['response']);
+
+        if (!$resp->is_valid) {
+            $this->addError($attribute,("The reCAPTCHA wasn't entered correctly. Go back and try it again." .
+                "(reCAPTCHA said: " . $resp->error . ")"));
+        }
     }
 
 
